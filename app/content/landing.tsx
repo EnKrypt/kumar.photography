8cb42ba5d7259d1dbc06ef7f57e7af7d4c308426
Arvind Kumar <arvind@enkrypt.in>
2026-09-20 20:11:19 +0530

    Update landing page and About Me section text

--- app/content/landing.tsx
+++ app/content/landing.tsx
@@ -1,20 +1,24 @@
-import type { Landing } from "~/lib/types";
+import type { Landing } from '~/lib/types';
 
 export const landing: Landing = {
   title: {
     node: (
       <>
-        <span className="title-first">arvind</span> <span className="title-last">kumar</span>
+        <span className="title-first">arvind</span>{' '}
+        <span className="title-last">kumar</span>
       </>
     ),
     subtitle: <>bird photography</>,
-    image: "2026-09-06-pied-bushchat",
-    overlay: "right",
+    image: '2026-09-06-pied-bushchat',
+    overlay: 'right'
   },
   images: [
-    "2026-08-16-pale-billed-flowerpecker-1",
-    "2026-08-16-yellow-billed-babbler-1",
-    "2026-08-16-blue-faced-malkoha",
-    "2026-08-09-little-egret",
-  ],
+    '2026-08-23-asian-green-bee-eater-2',
+    '2026-08-23-rose-ringed-parakeet',
+    '2026-08-23-tickells-blue-flycatcher',
+    '2026-08-16-pale-billed-flowerpecker-1',
+    '2026-08-16-yellow-billed-babbler-1',
+    '2026-08-16-blue-faced-malkoha',
+    '2026-08-09-little-egret'
+  ]
 };
